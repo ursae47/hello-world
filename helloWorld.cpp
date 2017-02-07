024e0e962bed6c94266980b0dafbbc7c7e4d9b81
Kevin <echo.controverc@gmail.com>
2017-02-07 13:59:28 -0600

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -9,7 +9,12 @@
 
 int main()
 {
-	std::cout << "hello world!\n";
-	system("pause");
+	std::cout << "hello world!\n";				//std::cout means that the program will display text to the console
+								//since we #include(ed)<iostream> technically we don't need the std::
+								//prefix - however some programmers enjoy the extra typing*cough*dad*
+	cout << "This works as well!" << endl;
+	system("pause");					//"system" invokes the command processor to issue some command - in this
+								//case it's telling the application to hold the text on the console screen
+								//until we get input from the keyboard.
 	return 0;
 }
