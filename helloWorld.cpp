e494e8fee2e8edad8fd6038d8fcc6fcf78683742
Kevin <echo.controverc@gmail.com>
2017-03-19 19:33:39 -0500

    test
    testtesttest

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,4 +1,6 @@
 
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                        Steve's hello world app in GitHub
@@ -18,5 +20,8 @@ int main()
 	system("pause");					//"system" invokes the command processor to issue some command - in this
 								//case it's telling the application to hold the text on the console screen
 								//until we get input from the keyboard.
+	
+	cout << "test" << endl;
+	
 	return 0;
 }
